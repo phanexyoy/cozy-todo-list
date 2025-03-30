6ec5383b5206ad137f33e2e43947815de39fd93a
Kevin Cha <143443005+kevincha12@users.noreply.github.com>
2025-03-30 09:28:45 -0400

    login details

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    return Account.query.get(int(user_id))
 
 @app.route('/')
 def index():
@@ -23,8 +33,20 @@ def index():
     else:
         todo_list = Todo.query.all()
 
-    return render_template('base.html', todo_list=todo_list, filter_val=filter_val)
+    return render_template('login.html', todo_list=todo_list, filter_val=filter_val)
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = Account.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for("index"))
+        else:
+            return render_template('login.html', error=True)
+    return render_template('login.html', error=False)
 
 
 @app.route('/add', methods=["POST"])
@@ -65,6 +87,7 @@ def delete(todo_id):
 
 
 if __name__ == '__main__':
+    login_manager.init_app(app)
     with app.app_context():
         db.create_all()
 
